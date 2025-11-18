ad11b783e37b6ca9c4e2240e518a47694ce543c2
Yuvadi29 <adityatrivedi612@gmail.com>
2025-11-18 21:06:24 +0530

    Adding Edges

--- src/components/ReactFlowComponent.jsx
+++ src/components/ReactFlowComponent.jsx
@@ -17,10 +17,21 @@ const initialNodes = [
   }
 ]
 
+// Creating edges
+const initialEdges = [
+  {
+    id: 'n1-n2', //Id should be usually given to connect from which node to which node
+    source: 'n1', //source node
+    target: 'n2', //destination node,
+    type: 'step',
+    label: 'connects with',
+  },
+];
+
 const ReactFlowComponent = () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} fitView>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} fitView>
         <Background />
         <Controls />
       </ReactFlow>
