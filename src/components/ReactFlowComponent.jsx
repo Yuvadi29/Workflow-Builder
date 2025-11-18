eb73616168049825fbf02425cb55b796241fee38
Yuvadi29 <adityatrivedi612@gmail.com>
2025-11-18 21:11:43 +0530

    Adding Interactivity in Nodes for State Management

--- src/components/ReactFlowComponent.jsx
+++ src/components/ReactFlowComponent.jsx
@@ -1,6 +1,12 @@
-import { ReactFlow, Controls, Background } from '@xyflow/react'
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  applyEdgeChanges,
+  applyNodeChanges
+} from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
-import React from 'react'
+import { useCallback, useState } from 'react'
 
 // Creating Initial Nodes
 const initialNodes = [
@@ -24,14 +30,32 @@ const initialEdges = [
     source: 'n1', //source node
     target: 'n2', //destination node,
     type: 'step',
-    label: 'connects with',
-  },
-];
+    label: 'connects with'
+  }
+]
 
 const ReactFlowComponent = () => {
+  const [nodes, setNodes] = useState(initialNodes)
+  const [edges, setEdges] = useState(initialEdges)
+
+  // Add Event handlers which will be used to update the state of our nodes and edges when changes occur like dragging or deleting element
+  const onNodesChange = useCallback(changes =>
+    setNodes(nodesSnapshot => applyNodeChanges(changes, nodesSnapshot), [])
+  )
+
+  const onEdgesChange = useCallback(changes =>
+    setEdges(edgeSnapshot => applyEdgeChanges(changes, edgeSnapshot), [])
+  )
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} fitView>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
